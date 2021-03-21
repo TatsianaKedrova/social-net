ae5e1741fbeefc06d6ea3f8090154e55431c47e0
Tatiana <Baletrot@gmail.com>
2021-03-21 13:51:43 +0200

    added BBl to Dialogs file

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import DialogItem from "./DialogItem";
 import classes from "./Dialogs.module.css";
 import Message from "./Message";
@@ -7,23 +6,43 @@ import Message from "./Message";
 
 	
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Tatiana'},
+        {id: 2, name: 'Sveta'},
+        {id: 3, name: 'Elena'},
+        {id: 4, name: 'Vladimir'},
+        {id: 5, name: 'Nadin'},
+        {id: 6, name: 'Sasha'},
+        {id: 7, name: 'Diana'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'What is your goal for next 3 years?'},
+        {id: 4, message: 'I plan to work as a FrontEnd developer remotely for US or Canada company.'},
+        {id: 5, message: 'Good. How is your sweetheart'}
+    ];
+
 	return (
 		<div className={classes.dialogs}>
 			<div className={classes.dialogsItems}>
-                <DialogItem name = {"Tatiana"} id = {1} />
-                <DialogItem name = {"Sveta"} id = {2}/>
-                <DialogItem name = {"Elena"} id = {3}/>
-                <DialogItem name = {"Vladimir"} id = {4}/>
-                <DialogItem name = {"Nadin"} id = {5}/>
-                <DialogItem name = {"Sasha"} id = {6}/>
-                <DialogItem name = {"Diana"} id = {7}/>
-                <DialogItem name = {"Sherif"} id = {8}/>
+                <DialogItem name = {dialogsData[0].name} id = {dialogsData[0].id} />
+                <DialogItem name = { dialogsData[1].name} id = {dialogsData[1].id}/>
+                <DialogItem name = { dialogsData[2].name} id = {dialogsData[2].id}/>
+                <DialogItem name = { dialogsData[3].name} id = {dialogsData[3].id}/>
+                <DialogItem name = { dialogsData[4].name} id = {dialogsData[4].id}/>
+                <DialogItem name = { dialogsData[5].name} id = {dialogsData[5].id}/>
+                <DialogItem name = { dialogsData[6].name} id = {dialogsData[6].id}/>
+                <DialogItem name = { dialogsData[7].name} id = {dialogsData[7].id}/>
 			</div>
 			<div className={classes.messages}>
-                <Message message={"Hello"}/>
-                <Message message={"How are you?"}/>
-                <Message message={"Great! My React Typescript job is waiting for me!"}/>
-                <Message message={"Consistency and persistence are the keys for success"}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
+                <Message message={messagesData[3].message}/>
+                <Message message={messagesData[4].message}/>
 			</div>
 		</div>
 	);
