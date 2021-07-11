c9fb9fd841904197551cd16c9d6ca34756c0c119
TatsianaKedrova <Baletrot@gmail.com>
2021-07-11 12:33:03 +0200

    removed UsersAPIConponent and united 2 container components together into one.

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -7,7 +7,45 @@ import {
     SingleUserType
 } from "../../redux/users-reducer";
 import {AppDispatch, AppRootType} from "../../redux/store-redux";
-import UsersAPIComponent from "./UsersAPIComponent";
+import React from "react";
+import axios from "axios";
+import {UserPresentational} from "./UserPresentational";
+
+export type UsersPropsType = {
+    users: Array<SingleUserType>
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
+    followUnfollow: (userId: number) => void
+    setUsers: (users: Array<SingleUserType>) => void
+    setTotalUsersCount: (count: number) => void
+    setCurrentPage: (page: number) => void
+}
+
+class UsersAPIComponent extends React.Component<UsersPropsType> {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                    console.log(response)
+                    this.props.setUsers(response.data.items);
+                    this.props.setTotalUsersCount(response.data.totalCount);
+
+                }
+            )}
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+            }
+        )
+    }
+    render() {
+
+        return <UserPresentational users={this.props.users} pageSize={this.props.pageSize} totalUsersCount={this.props.totalUsersCount} currentPage={this.props.currentPage} followUnfollow={this.props.followUnfollow} onPageChanged={this.onPageChanged} />
+    }
+}
 
 let mapStateToProps = (state: AppRootType) => {
 
