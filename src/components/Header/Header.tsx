3c4b64f176954ee869230ddf8b572787bd1909f0
TatsianaKedrova <Baletrot@gmail.com>
2021-11-25 13:59:36 +0200

    added name of social net and corrected navbar styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
 import { NavLink } from "react-router-dom";
-import authUserPhoto from '../../assets/authUserPhoto.jpg'; 
+import authUserPhoto from "../../assets/authUserPhoto.jpg";
 
 export type HeaderPresentationalPropsType = {
-    isAuth: boolean
+  isAuth: boolean;
+  login: string | null;
+  setAuthUserData: (
+    userId: number | null,
+    email: string | null,
     login: string | null
-    setAuthUserData: (userId: number | null, email: string | null,login: string | null) => void
-}
+  ) => void;
+};
 
-const Header = (props: HeaderPresentationalPropsType) => {
-    return (
-
-            <header className={classes.header}>
-				<img src={authUserPhoto}
-                alt='headerImage'/>
-                <div className={classes.loginBlock}>
-                    { props.isAuth ? props.login :
-                    <NavLink to={'/login'} style={{"textDecoration": "none", "color": "white"}}>Login</NavLink>}
-                </div>
-			</header>
-    )
-}
+const Header: React.FC<HeaderPresentationalPropsType> = ({
+  isAuth,
+  login,
+  setAuthUserData,
+}) => {
+  return (
+    <header className={classes.header}>
+      <span className={classes.socialNetStyle}>Baletro SocialNet</span>
+      <img src={authUserPhoto} alt="headerImage" />
+      <div className={classes.loginBlock}>
+        {isAuth ? (
+          login
+        ) : (
+          <NavLink
+            to={"/login"}
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Login
+          </NavLink>
+        )}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,49 +5,42 @@ import ScrollArea from "react-scrollbar";
 
 const Navbar = () => {
   return (
-	<ScrollArea vertical smoothScrolling>
-    <nav className={classes.nav}>
-      <div className={classes.item}>
-        <NavLink to="/profile" activeClassName={classes.active}>
-          Profile
-        </NavLink>
-      </div>
-      <br />
+    <ScrollArea vertical smoothScrolling>
+      <nav className={classes.nav}>
+        <div className={classes.item}>
+          <NavLink to="/profile" activeClassName={classes.active}>
+            My Profile
+          </NavLink>
+        </div>
+        <div className={classes.item}>
+          <NavLink to="/users" activeClassName={classes.active}>
+            Users
+          </NavLink>
+        </div>
 
-      <div className={classes.item}>
-        <NavLink to="/users" activeClassName={classes.active}>
-          Users
-        </NavLink>
-      </div>
-      <br />
+        <div className={classes.item}>
+          <NavLink to="/dialogs" activeClassName={classes.active}>
+            Messages
+          </NavLink>
+        </div>
 
-      <div className={classes.item}>
-        <NavLink to="/dialogs" activeClassName={classes.active}>
-          Messages
-        </NavLink>
-      </div>
-      <br />
-
-      <div className={classes.item}>
-        <NavLink to="/news" activeClassName={classes.active}>
-          News
-        </NavLink>
-      </div>
-      <br />
-      <div className={classes.item}>
-        <NavLink to="/music" activeClassName={classes.active}>
-          Music
-        </NavLink>
-      </div>
-      <br />
-      <div className={classes.item}>
-        <NavLink to="/settings" activeClassName={classes.active}>
-          Settings
-        </NavLink>
-      </div>
-    </nav>
-	</ScrollArea>
-
+        <div className={classes.item}>
+          <NavLink to="/news" activeClassName={classes.active}>
+            News
+          </NavLink>
+        </div>
+        <div className={classes.item}>
+          <NavLink to="/music" activeClassName={classes.active}>
+            Music
+          </NavLink>
+        </div>
+        <div className={classes.item}>
+          <NavLink to="/settings" activeClassName={classes.active}>
+            Settings
+          </NavLink>
+        </div>
+      </nav>
+    </ScrollArea>
   );
 };
 export default Navbar;
